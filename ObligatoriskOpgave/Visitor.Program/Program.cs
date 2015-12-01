93fbc4e99981c81eaeb799d4292fa37dfbafb7e3
Kalle Møller <github.com@k-moeller.dk>
2015-12-01 15:35:16 +0100

    Example

--- ObligatoriskOpgave/Visitor.Program/Program.cs
+++ ObligatoriskOpgave/Visitor.Program/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VisitorImplementation;
+using VisitorAsset;
 
 namespace Visitor.Program
 {
@@ -11,31 +12,53 @@ namespace Visitor.Program
     {
         static void Main(string[] args)
         {
-            var Employees = new Employees();
 
-            var Emp1 = new Employee("Kalle", 5.400, 20, 100);
-            var Emp2 = new Employee("Kasper", 15293.1, 40, 5);
-            var Emp3 = new Employee("Kristian", 100.0, 14, 10);
-            var Emp4 = new Leader("Peter", 500.0, 20, 130);
+            var rooms = new CompositeAsset();
 
-            Employees.AddEmployee(Emp1);
-            Employees.AddEmployee(Emp2);
-            Employees.AddEmployee(Emp3);
-            Employees.AddEmployee(Emp4);
+            rooms.Add(new Room { MonthlyRent = 10000 });
+            rooms.Add(new Room { MonthlyRent = 15000 });
+            rooms.Add(new Room { MonthlyRent = 9000 });
 
-            var SalaryRepport = new SalaryRepport();
-            var VacationRepport = new VacationRepport();
-            var SickRepport = new SickRepport();
+            var gruppe1 = new CompositeAsset();
 
-            Employees.Accept(SalaryRepport);
-            Console.WriteLine("");
-            Employees.Accept(VacationRepport);
-            Console.WriteLine("");
-            Employees.Accept(SickRepport);
+            gruppe1.Add(new CEO {HiredDate = new DateTime(), HoursWorkedThisMonth = 300, MontlySalary = 100000});
+            gruppe1.Add(new StudentEmployee {HiredDate = new DateTime(), HourRate = 120, HoursWorkedThisMonth = 100});
+            gruppe1.Add(new StudentEmployee { HiredDate = new DateTime(), HourRate = 10, HoursWorkedThisMonth = 400 });
 
-            Emp4.Accept(SalaryRepport);
+            var assets = new CompositeAsset();
+            assets.Add(rooms);
+            assets.Add(gruppe1);
 
-            Console.ReadKey();
+            var monthlycacl = new MonthlyExpenses();
+            monthlycacl.ShowMonthly(assets);
+
+            var dates = new HiredDates();
+            dates.PrintDates(assets);
+            //var Employees = new Employees();
+
+            //var Emp1 = new Employee("Kalle", 5.400, 20, 100);
+            //var Emp2 = new Employee("Kasper", 15293.1, 40, 5);
+            //var Emp3 = new Employee("Kristian", 100.0, 14, 10);
+            //var Emp4 = new Leader("Peter", 500.0, 20, 130);
+
+            //Employees.AddEmployee(Emp1);
+            //Employees.AddEmployee(Emp2);
+            //Employees.AddEmployee(Emp3);
+            //Employees.AddEmployee(Emp4);
+
+            //var SalaryRepport = new SalaryRepport();
+            //var VacationRepport = new VacationRepport();
+            //var SickRepport = new SickRepport();
+
+            //Employees.Accept(SalaryRepport);
+            //Console.WriteLine("");
+            //Employees.Accept(VacationRepport);
+            //Console.WriteLine("");
+            //Employees.Accept(SickRepport);
+
+            //Emp4.Accept(SalaryRepport);
+
+            //Console.ReadKey();
         }
     }
 }
---
+++ ObligatoriskOpgave/VisitorAsset/Class1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorAsset
+{
+    public interface IVisitor
+    {
+        void Visit(Room room);
+        void Visit(Employee employee);
+        void Visit(StudentEmployee employee);
+        void Visit(CEO ceo);
+    }
+
+    public class MonthlyExpenses : IVisitor
+    {
+        public void Visit(Room room)
+        {
+            _expenses += room.MonthlyRent;
+        }
+
+        public void Visit(Employee employee)
+        {
+            
+        }
+
+        public void Visit(StudentEmployee employee)
+        {
+            _expenses += employee.HourRate*employee.HoursWorkedThisMonth;
+        }
+
+        public void Visit(CEO ceo)
+        {
+            _expenses += ceo.MontlySalary;
+        }
+
+        private int _expenses;
+
+        public MonthlyExpenses()
+        {
+            _expenses = 0;
+        }
+
+        public void ShowMonthly(IAsset asset)
+        {
+            _expenses = 0;
+            asset.Accept(this);
+            Console.WriteLine("Monthly expenses: " + _expenses);
+        }
+    }
+
+
+    public class HiredDates : IVisitor
+    {
+        public void Visit(Room room)
+        {
+            
+        }
+
+        public void Visit(Employee employee)
+        {
+            Console.WriteLine("Empl: " + employee.HiredDate);
+        }
+
+        public void Visit(StudentEmployee employee)
+        {
+            Console.WriteLine("Student: " + employee.HiredDate);
+        }
+
+        public void Visit(CEO ceo)
+        {
+            Console.WriteLine("Ceo: " + ceo.HiredDate);
+        }
+
+        public void PrintDates(IAsset assets)
+        {
+            assets.Accept(this);
+        }
+    }
+    public interface IAsset
+    {
+        void Accept(IVisitor visitor);
+    }
+
+    public class Room : IAsset
+    {
+        public int MonthlyRent { get; set; }
+        public void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    public class Employee : IAsset
+    {
+        public virtual DateTime HiredDate { get; set; }
+
+        public virtual int HoursWorkedThisMonth { get; set; }
+        public virtual void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    public class StudentEmployee : Employee
+    {
+        public int HourRate { get; set; }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    public class CEO : Employee
+    {
+        public int MontlySalary { get; set; }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    public class CompositeAsset : IAsset
+    {
+        private readonly List<IAsset> _assets;
+
+        public CompositeAsset()
+        {
+            _assets = new List<IAsset>();
+        }
+
+        public void Add(IAsset asset)
+        {
+            _assets.Add(asset);
+        }
+
+        public void Accept(IVisitor visitor)
+        {
+            foreach (var asset in _assets)
+            {
+                asset.Accept(visitor);
+            }
+        }
+
+
+    }
+}
